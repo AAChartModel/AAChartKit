08d8a5ec7d018c771787bd33b429c2b2d096a578
李照闫 <2236368544@qq.com>
2017-07-03 16:47:10 +0800

    Correct the naming notations of enumeration

--- AAChartKit/ChartsDemo/SecondViewController.h
+++ AAChartKit/ChartsDemo/SecondViewController.h
@@ -8,20 +8,20 @@
 //
 
 #import <UIKit/UIKit.h>
-typedef NS_ENUM(NSInteger,ENUM_secondeViewController_chartType){
-    ENUM_secondeViewController_chartTypeColumn =0,
-    ENUM_secondeViewController_chartTypeBar,
-    ENUM_secondeViewController_chartTypeArea,
-    ENUM_secondeViewController_chartTypeAreaspline,
-    ENUM_secondeViewController_chartTypeLine,
-    ENUM_secondeViewController_chartTypeSpline,
-    ENUM_secondeViewController_chartTypeScatter,
+typedef NS_ENUM(NSInteger,SecondeViewControllerChartType){
+    SecondeViewControllerChartTypeColumn =0,
+    SecondeViewControllerChartTypeBar,
+    SecondeViewControllerChartTypeArea,
+    SecondeViewControllerChartTypeAreaspline,
+    SecondeViewControllerChartTypeLine,
+    SecondeViewControllerChartTypeSpline,
+    SecondeViewControllerChartTypeScatter,
     
 };
 
 
 @interface SecondViewController : UIViewController
-@property(nonatomic,assign)NSInteger ENUM_secondeViewController_chartType;
+@property(nonatomic,assign)NSInteger SecondeViewControllerChartType;
 @property(nonatomic,copy)NSString *receivedChartType;
 
 @end
