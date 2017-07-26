764bb91328745fbdcef81faee7cba5facd8581dd
李照闫 <2236368544@qq.com>
2017-07-26 14:45:15 +0800

    Append the link of source code

--- AAChartKitLib/AAJSFiles/AAHighcharts-3d.js
+++ AAChartKitLib/AAJSFiles/AAHighcharts-3d.js
@@ -1,3 +1,9 @@
+//
+//  Created by An An on 17/1/9.
+//  Copyright © 2017年 An An. All rights reserved.
+//  source code ----*** https://github.com/AAChartModel/AAChartKit ***--- source code
+//
+
 /*
  Highcharts JS v5.0.6 (2016-12-07)
  
