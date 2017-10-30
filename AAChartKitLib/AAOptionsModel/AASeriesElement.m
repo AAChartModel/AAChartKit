395c47a565ff17a0798dd049f8fedb56b6de94c6
李照闫 <2236368544@qq.com>
2017-10-30 16:27:13 +0800

    Append the `WARM TIPS`

--- AAChartKitLib/AAOptionsModel/AASeriesElement.m
+++ AAChartKitLib/AAOptionsModel/AASeriesElement.m
@@ -6,6 +6,26 @@
 //  Copyright © 2017年 An An. xAll rights reserved.
 //
 
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ * ❀❀❀   WARM TIPS!!!   ❀❀❀
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit-Swift/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/7842508/codeforu
+ * JianShu       : http://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
+
+
 #import "AASeriesElement.h"
 
 @implementation AASeriesElement
