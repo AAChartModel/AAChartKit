cb00d843e704eccedcd115922cb434da848b3cc7
An An <2236368544@qq.com>
2017-11-29 23:12:02 +0800

    Correct the legend layout default style

--- AAChartKitLib/AAChartConfiger/AAOptionsConstructor.m
+++ AAChartKitLib/AAChartConfiger/AAOptionsConstructor.m
@@ -135,7 +135,7 @@
     
     AALegend *legend = AAObject(AALegend)
     .enabledSet(chartModel.legendEnabled)//是否显示 legend
-    .layoutSet(AALegendLayoutTypeVertical)//图例数据项的布局。布局类型： "horizontal" 或 "vertical" 即水平布局和垂直布局 默认是：horizontal.
+    .layoutSet(AALegendLayoutTypeHorizontal)//图例数据项的布局。布局类型： "horizontal" 或 "vertical" 即水平布局和垂直布局 默认是：horizontal.
     .alignSet(AALegendAlignTypeCenter)//设定图例在图表区中的水平对齐方式，合法值有left，center 和 right。
     .verticalAlignSet(AALegendVerticalAlignTypeBottom)//设定图例在图表区中的垂直对齐方式，合法值有 top，middle 和 bottom。垂直位置可以通过 y 选项做进一步设定。
     .itemMarginTopSet(@0);//图例的每一项的顶部外边距，单位px。 默认是：0.
