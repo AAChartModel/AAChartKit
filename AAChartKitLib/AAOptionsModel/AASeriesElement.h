041fa2e5e7abd41b81d588f75061ee72ae100b49
skuske <skuske@totalidea.com>
2018-01-19 10:22:55 +0100

    Update AASeriesElement.h
    keys support

--- AAChartKitLib/AAOptionsModel/AASeriesElement.h
+++ AAChartKitLib/AAOptionsModel/AASeriesElement.h
@@ -40,5 +40,6 @@ AAPropStatementAndFuncStatement(strong, AASeriesElement, NSArray  *, data);
 AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, innerSize);
 AAPropStatementAndFuncStatement(strong, AASeriesElement, AAMarker *, marker);
 AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, stacking);
+AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSArray  *, keys);
 
 @end
