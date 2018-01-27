193492bbb3c117d0672496e70e245464f4236344
An An <2236368544@qq.com>
2018-01-27 21:26:08 +0800

    Fix #137

--- AAChartKitLib/AAOptionsModel/AASeriesElement.h
+++ AAChartKitLib/AAOptionsModel/AASeriesElement.h
@@ -33,14 +33,15 @@
 
 @interface AASeriesElement : NSObject
 
-AAPropStatementAndFuncStatement(strong, AASeriesElement, NSNumber *, borderRadius);
 AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, type);
 AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, name);
 AAPropStatementAndFuncStatement(strong, AASeriesElement, NSArray  *, data);
-AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, innerSize);
 AAPropStatementAndFuncStatement(strong, AASeriesElement, AAMarker *, marker);
 AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, stacking);
 AAPropStatementAndFuncStatement(strong, AASeriesElement, NSNumber *, threshold);
-
+AAPropStatementAndFuncStatement(strong, AASeriesElement, NSNumber *, lineWidth);//折线图、曲线图、直方折线图、折线填充图、曲线填充图、直方折线填充图的线条宽度
+AAPropStatementAndFuncStatement(strong, AASeriesElement, NSNumber *, fillOpacity);//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色透明度
+AAPropStatementAndFuncStatement(strong, AASeriesElement, NSNumber *, borderRadius);
+AAPropStatementAndFuncStatement(copy,   AASeriesElement, NSString *, innerSize);
 
 @end
--- AAChartKitLib/AAOptionsModel/AASeriesElement.m
+++ AAChartKitLib/AAOptionsModel/AASeriesElement.m
@@ -29,15 +29,16 @@
 #import "AASeriesElement.h"
 
 @implementation AASeriesElement
-
-AAPropSetFuncImplementation(AASeriesElement, NSNumber *, borderRadius);
+//threshold
 AAPropSetFuncImplementation(AASeriesElement, NSString *, type);
 AAPropSetFuncImplementation(AASeriesElement, NSString *, name);
 AAPropSetFuncImplementation(AASeriesElement, NSArray  *, data);
-AAPropSetFuncImplementation(AASeriesElement, NSString *, innerSize);
 AAPropSetFuncImplementation(AASeriesElement, AAMarker *, marker);
 AAPropSetFuncImplementation(AASeriesElement, NSString *, stacking);
 AAPropSetFuncImplementation(AASeriesElement, NSNumber *, threshold);
-
+AAPropSetFuncImplementation(AASeriesElement, NSNumber *, lineWidth);//折线图、曲线图、直方折线图、折线填充图、曲线填充图、直方折线填充图的线条宽度
+AAPropSetFuncImplementation(AASeriesElement, NSNumber *, fillOpacity);//折线填充图、曲线填充图、直方折线填充图等填充图类型的填充颜色透明度
+AAPropSetFuncImplementation(AASeriesElement, NSNumber *, borderRadius);
+AAPropSetFuncImplementation(AASeriesElement, NSString *, innerSize);
 
 @end
