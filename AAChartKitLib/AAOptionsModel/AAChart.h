2ce1d328b98c79fa064d780a74729c50332f3716
An An <2236368544@qq.com>
2018-02-05 11:19:48 +0800

    Delete useless properties for AAChart

--- AAChartKitLib/AAOptionsModel/AAChart.h
+++ AAChartKitLib/AAOptionsModel/AAChart.h
@@ -42,23 +42,10 @@ AAPropStatementAndFuncStatement(copy,   AAChart, NSString    *, backgroundColor)
 AAPropStatementAndFuncStatement(copy,   AAChart, NSString    *, pinchType);
 AAPropStatementAndFuncStatement(assign, AAChart, BOOL,          panning);
 AAPropStatementAndFuncStatement(copy,   AAChart, NSString    *, panKey);
-AAPropStatementAndFuncStatement(copy,   AAChart, NSString    *, plotBackgroundColor);
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, plotBorderWidth);
-AAPropStatementAndFuncStatement(assign, AAChart, BOOL,          plotShadow);
 AAPropStatementAndFuncStatement(assign, AAChart, BOOL,          polar);
 AAPropStatementAndFuncStatement(strong, AAChart, AAOptions3d *, options3d);
 AAPropStatementAndFuncStatement(assign, AAChart, AAAnimation *, animation);//设置启用动画的时间和类型
 AAPropStatementAndFuncStatement(assign, AAChart, BOOL,          inverted);
-//AAPropStatementAndFuncStatement(strong, AAChart, NSArray *, spacing);//图表的内边距，指图表外边缘和绘图区之间的距离，数组中的数字分别表示顶部，右侧，底部和左侧。可以使用选项 spacingTop，spacingRight，spacingBottom 和 spacingLeft 来指定某一个内边距。 默认是：[10, 10, 15, 10].
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, spacingBottom);//图表的底部内边距（图表底部边缘与内容的距离）。
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, spacingLeft);
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, spacingRight);
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, spacingTop);
-
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, marginBottom);//图表底部外边缘和绘图区域之间的边距。另请参见spacingBottom
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, marginLeft);
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, marginRight);
-AAPropStatementAndFuncStatement(strong, AAChart, NSNumber    *, marginTop);
 
 @end
 
--- AAChartKitLib/AAOptionsModel/AAChart.m
+++ AAChartKitLib/AAOptionsModel/AAChart.m
@@ -34,41 +34,14 @@
 
 @implementation AAChart
 
-//-(instancetype)init{
-//    self = [super init];
-//    if (self) {
-//        self.spacingTop = @0;
-//        self.spacingLeft =@0;
-//        self.spacingRight = @0;
-//        self.spacingBottom = @0;
-//        self.marginBottom = @40;
-//        self.marginLeft = @0;
-//        self.marginRight = @0;
-//        self.marginTop = 0;
-//     }
-//    return self;
-//}
-
 AAPropSetFuncImplementation(AAChart, NSString    *, type);
 AAPropSetFuncImplementation(AAChart, NSString    *, backgroundColor);
 AAPropSetFuncImplementation(AAChart, NSString    *, pinchType);
 AAPropSetFuncImplementation(AAChart, BOOL ,         panning);
 AAPropSetFuncImplementation(AAChart, NSString    *, panKey);
-AAPropSetFuncImplementation(AAChart, NSString    *, plotBackgroundColor);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, plotBorderWidth);
-AAPropSetFuncImplementation(AAChart, BOOL,          plotShadow);
 AAPropSetFuncImplementation(AAChart, BOOL ,         polar);
 AAPropSetFuncImplementation(AAChart, AAOptions3d *, options3d);
 AAPropSetFuncImplementation(AAChart, AAAnimation *, animation);
 AAPropSetFuncImplementation(AAChart, BOOL ,         inverted);
-//AAPropSetFuncImplementation(AAChart, NSArray  *, spacing);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, spacingBottom);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, spacingLeft);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, spacingRight);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, spacingTop);
 
-AAPropSetFuncImplementation(AAChart, NSNumber    *, marginBottom);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, marginLeft);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, marginRight);
-AAPropSetFuncImplementation(AAChart, NSNumber    *, marginTop);
 @end
