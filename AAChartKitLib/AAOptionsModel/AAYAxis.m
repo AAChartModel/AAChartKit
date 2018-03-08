13f2a97f93284d650688ba39d52b75eda10942ae
Stefan Kuske <skuske@totalidea.com>
2018-03-08 08:00:23 +0100

    Update AAYAxis.m

--- AAChartKitLib/AAOptionsModel/AAYAxis.m
+++ AAChartKitLib/AAOptionsModel/AAYAxis.m
@@ -52,7 +52,6 @@ AAPropSetFuncImplementation(AAYAxis, NSNumber *, min); //y轴最小值（设置�
 AAPropSetFuncImplementation(AAYAxis, NSArray  *, tickPositions);//自定义Y轴坐标（如：[@(0), @(25), @(50), @(75) , (100)]）
 AAPropSetFuncImplementation(AAYAxis, BOOL,       visible); //y轴是否允许显示
 AAPropSetFuncImplementation(AAYAxis, BOOL,       opposite);//是否将坐标轴显示在对立面，默认情况下 x 轴是在图表的下方显示，y 轴是在左方，坐标轴显示在对立面后，x 轴是在上方显示，y 轴是在右方显示（即坐标轴会显示在对立面）。该配置一般是用于多坐标轴区分展示，另外在 Highstock 中，y 轴默认是在对立面显示的。 默认是：false.
-
-
+AAPropSetFuncImplementation(AAYAxis, NSNumber *, tickInterval);
 
 @end
