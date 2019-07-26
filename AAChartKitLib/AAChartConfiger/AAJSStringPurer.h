8d3b327973cb04c47857fd1ede3751a09307f646
AnAn <2236368544@qq.com>
2019-07-26 11:27:08 +0800

    Add WARM TIPS for AAJSStringPuer

--- AAChartKitLib/AAChartConfiger/AAJSStringPurer.h
+++ AAChartKitLib/AAChartConfiger/AAJSStringPurer.h
@@ -4,7 +4,31 @@
 //
 //  Created by AnAn on 2019/7/17.
 //  Copyright © 2019 Danny boy. All rights reserved.
-//
+//*************** ...... SOURCE CODE ...... ***************
+//***...................................................***
+//*** https://github.com/AAChartModel/AAChartKit        ***
+//*** https://github.com/AAChartModel/AAChartKit-Swift  ***
+//***...................................................***
+//*************** ...... SOURCE CODE ...... ***************
+
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ * 🌕 🌖 🌗 🌘  ❀❀❀   WARM TIPS!!!   ❀❀❀ 🌑 🌒 🌓 🌔
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/7842508/codeforu
+ * JianShu       : https://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
 
 #import <Foundation/Foundation.h>
 
