d9dce8ac64573abce25f42226dc951f242642e20
AnAn <2236368544@qq.com>
2019-09-04 14:36:59 +0800

    Optimize AAOptionsConstructor

--- AAChartKitLib/AAChartConfiger/AAOptions.m
+++ AAChartKitLib/AAChartConfiger/AAOptions.m
@@ -100,7 +100,7 @@ AAPropSetFuncImplementation(AAOptions, BOOL           , touchEventEnabled)
     }
     
     [self configureTheStyleOfConnectNodeWithChartModel:aaChartModel plotOptions:aaPlotOptions];
-    aaPlotOptions = [self configureTheAAPlotOptionsWithPlotOptions:aaPlotOptions chartModel:aaChartModel];
+    [self configureTheAAPlotOptionsWithPlotOptions:aaPlotOptions chartModel:aaChartModel];
     
     AALegend *aaLegend = AALegend.new
     .enabledSet(aaChartModel.legendEnabled);//是否显示 legend
@@ -118,75 +118,13 @@ AAPropSetFuncImplementation(AAOptions, BOOL           , touchEventEnabled)
     .zoomResetButtonTextSet(aaChartModel.zoomResetButtonText)//重置缩放按钮的默认标题
     .touchEventEnabledSet(aaChartModel.touchEventEnabled);//是否支持点击事件
     
-    if (   aaChartModel.chartType != AAChartTypePie
-        && aaChartModel.chartType != AAChartTypePyramid
-        && aaChartModel.chartType != AAChartTypeFunnel) {
-        [self configureAxisContentAndStyleWithAAOptions:aaOptions AAChartModel:aaChartModel];
-    }
+    [self configureAxisContentAndStyleWithAAOptions:aaOptions AAChartModel:aaChartModel];
     
     return aaOptions;
 }
 
-+ (void)configureAxisContentAndStyleWithAAOptions:(AAOptions *)aaOptions AAChartModel:(AAChartModel *)aaChartModel {
-    
-    AAXAxis *aaXAxis = AAXAxis.new
-    .labelsSet(AALabels.new
-               .enabledSet(aaChartModel.xAxisLabelsEnabled)//设置 x 轴是否显示文字
-               .styleSet(AAStyle.new
-                         .colorSet(aaChartModel.xAxisLabelsFontColor)//xAxis Label font color
-                         .fontSizeSet(AAFontSizeFormat(aaChartModel.xAxisLabelsFontSize))//xAxis Label font size
-                         .fontWeightSet(aaChartModel.xAxisLabelsFontWeight)//xAxis Label font weight
-                         )
-               )
-    .reversedSet(aaChartModel.xAxisReversed)
-    .gridLineWidthSet(aaChartModel.xAxisGridLineWidth)//x轴网格线宽度
-    .categoriesSet(aaChartModel.categories)
-    .visibleSet(aaChartModel.xAxisVisible)//x轴是否可见
-    .tickIntervalSet(aaChartModel.xAxisTickInterval);//x轴坐标点间隔数
-    
-    if ([aaChartModel.xAxisCrosshairWidth floatValue]>0) {
-        aaXAxis.crosshairSet(AACrosshair.new
-                             .widthSet(aaChartModel.xAxisCrosshairWidth)
-                             .colorSet(aaChartModel.xAxisCrosshairColor)
-                             .dashStyleSet(aaChartModel.xAxisCrosshairDashStyleType)
-                             );
-    }
-    
-    AAYAxis *aaYAxis = AAYAxis.new
-    .labelsSet(AALabels.new
-               .enabledSet(aaChartModel.yAxisLabelsEnabled)//设置 y 轴是否显示数字
-               .styleSet(AAStyle.new
-                         .colorSet(aaChartModel.yAxisLabelsFontColor)//yAxis Label font color
-                         .fontSizeSet(AAFontSizeFormat(aaChartModel.yAxisLabelsFontSize))//yAxis Label font size
-                         .fontWeightSet(aaChartModel.yAxisLabelsFontWeight)//yAxis Label font weight
-                         )
-               .formatSet(@"{value:.,0f}")//让y轴的值完整显示 而不是100000显示为100k
-               )
-    .minSet(aaChartModel.yAxisMin)//设置 y 轴最小值,最小值等于零就不能显示负值了
-    .maxSet(aaChartModel.yAxisMax)//y轴最大值
-    .tickPositionsSet(aaChartModel.yAxisTickPositions)//自定义Y轴坐标
-    .allowDecimalsSet(aaChartModel.yAxisAllowDecimals)//是否允许显示小数
-    .plotLinesSet(aaChartModel.yAxisPlotLines) //标示线设置
-    .reversedSet(aaChartModel.yAxisReversed)
-    .gridLineWidthSet(aaChartModel.yAxisGridLineWidth)//y轴网格线宽度
-    .titleSet(AAAxisTitle.new.textSet(aaChartModel.yAxisTitle))//y 轴标题
-    .lineWidthSet(aaChartModel.yAxisLineWidth)//设置 y轴轴线的宽度,为0即是隐藏 y轴轴线
-    .visibleSet(aaChartModel.yAxisVisible)
-    .tickIntervalSet(aaChartModel.yAxisTickInterval);
-    
-    if ([aaChartModel.yAxisCrosshairWidth floatValue]>0) {
-        aaYAxis.crosshairSet(AACrosshair.new
-                             .widthSet(aaChartModel.yAxisCrosshairWidth)
-                             .colorSet(aaChartModel.yAxisCrosshairColor)
-                             .dashStyleSet(aaChartModel.yAxisCrosshairDashStyleType)
-                             );
-    }
-    
-    aaOptions.xAxis = aaXAxis;
-    aaOptions.yAxis = aaYAxis;
-}
-
-+ (void)configureTheStyleOfConnectNodeWithChartModel:(AAChartModel *)aaChartModel plotOptions:(AAPlotOptions *)aaPlotOptions {
++ (void)configureTheStyleOfConnectNodeWithChartModel:(AAChartModel *)aaChartModel
+                                         plotOptions:(AAPlotOptions *)aaPlotOptions {
     //数据点标记相关配置，只有折线图、曲线图、折线区域填充图、曲线区域填充图、散点图才有数据点标记
     if (   aaChartModel.chartType == AAChartTypeArea
         || aaChartModel.chartType == AAChartTypeAreaspline
@@ -210,20 +148,20 @@ AAPropSetFuncImplementation(AAOptions, BOOL           , touchEventEnabled)
     }
 }
 
-+ (AAPlotOptions *)configureTheAAPlotOptionsWithPlotOptions:(AAPlotOptions *)aaPlotOptions chartModel:(AAChartModel *)aaChartModel {
++ (void)configureTheAAPlotOptionsWithPlotOptions:(AAPlotOptions *)aaPlotOptions
+                                      chartModel:(AAChartModel *)aaChartModel {
     
     AAChartType chartType = aaChartModel.chartType;
     
     AADataLabels *aaDataLabels;
     if (aaChartModel.dataLabelsEnabled == true) {
-        aaDataLabels = (AADataLabels.new
-                        .enabledSet(aaChartModel.dataLabelsEnabled)
-                        .styleSet(AAStyle.new
-                                  .colorSet(aaChartModel.dataLabelsFontColor)
-                                  .fontSizeSet(AAFontSizeFormat(aaChartModel.dataLabelsFontSize))
-                                  .fontWeightSet(aaChartModel.dataLabelsFontWeight)
-                                  )
-                        );
+        aaDataLabels = AADataLabels.new
+        .enabledSet(aaChartModel.dataLabelsEnabled)
+        .styleSet(AAStyle.new
+                  .colorSet(aaChartModel.dataLabelsFontColor)
+                  .fontSizeSet(AAFontSizeFormat(aaChartModel.dataLabelsFontSize))
+                  .fontWeightSet(aaChartModel.dataLabelsFontWeight)
+                  );
     }
     
     if (chartType == AAChartTypeColumn) {
@@ -255,17 +193,14 @@ AAPropSetFuncImplementation(AAOptions, BOOL           , touchEventEnabled)
     } else if (chartType == AAChartTypeSpline) {
         aaPlotOptions.splineSet(AASpline.new.dataLabelsSet(aaDataLabels));
     } else if (chartType == AAChartTypePie) {
-        AAPie *aaPie = (AAPie.new
-                        .allowPointSelectSet(true)
-                        .cursorSet(@"pointer")
-                        .showInLegendSet(true)
-                        );
+        AAPie *aaPie = AAPie.new
+        .allowPointSelectSet(true)
+        .cursorSet(@"pointer")
+        .showInLegendSet(true);
         if (aaChartModel.dataLabelsEnabled == true) {
-            aaPie.dataLabelsSet(aaDataLabels.formatSet(@"<b>{point.name}</b>: {point.percentage:.1f} %"));
-        } else {
-            aaPie.dataLabelsSet(AADataLabels.new.enabledSet(false));
+            aaDataLabels.formatSet(@"<b>{point.name}</b>: {point.percentage:.1f} %");
         }
-        aaPlotOptions.pieSet(aaPie);
+        aaPlotOptions.pieSet(aaPie.dataLabelsSet(aaDataLabels));
     } else if (chartType == AAChartTypeColumnrange) {
         NSMutableDictionary *columnRangeDic = [[NSMutableDictionary alloc]init];
         [columnRangeDic setValue:@0 forKey:@"borderRadius"];//The color of the border surrounding each column or bar
@@ -277,9 +212,71 @@ AAPropSetFuncImplementation(AAOptions, BOOL           , touchEventEnabled)
         [areaRangeDic setValue:aaDataLabels forKey:@"dataLabels"];
         aaPlotOptions.arearangeSet(areaRangeDic);
     }
-    return aaPlotOptions;
 }
 
++ (void)configureAxisContentAndStyleWithAAOptions:(AAOptions *)aaOptions
+                                     AAChartModel:(AAChartModel *)aaChartModel {
+    
+    if (   aaChartModel.chartType != AAChartTypePie
+        && aaChartModel.chartType != AAChartTypePyramid
+        && aaChartModel.chartType != AAChartTypeFunnel) {
+        AAXAxis *aaXAxis = AAXAxis.new
+        .labelsSet(AALabels.new
+                   .enabledSet(aaChartModel.xAxisLabelsEnabled)//设置 x 轴是否显示文字
+                   .styleSet(AAStyle.new
+                             .colorSet(aaChartModel.xAxisLabelsFontColor)//xAxis Label font color
+                             .fontSizeSet(AAFontSizeFormat(aaChartModel.xAxisLabelsFontSize))//xAxis Label font size
+                             .fontWeightSet(aaChartModel.xAxisLabelsFontWeight)//xAxis Label font weight
+                             )
+                   )
+        .reversedSet(aaChartModel.xAxisReversed)
+        .gridLineWidthSet(aaChartModel.xAxisGridLineWidth)//x轴网格线宽度
+        .categoriesSet(aaChartModel.categories)
+        .visibleSet(aaChartModel.xAxisVisible)//x轴是否可见
+        .tickIntervalSet(aaChartModel.xAxisTickInterval);//x轴坐标点间隔数
+        
+        if (aaChartModel.xAxisCrosshairWidth > 0) {
+            aaXAxis.crosshairSet(AACrosshair.new
+                                 .widthSet(aaChartModel.xAxisCrosshairWidth)
+                                 .colorSet(aaChartModel.xAxisCrosshairColor)
+                                 .dashStyleSet(aaChartModel.xAxisCrosshairDashStyleType)
+                                 );
+        }
+        
+        AAYAxis *aaYAxis = AAYAxis.new
+        .labelsSet(AALabels.new
+                   .enabledSet(aaChartModel.yAxisLabelsEnabled)//设置 y 轴是否显示数字
+                   .styleSet(AAStyle.new
+                             .colorSet(aaChartModel.yAxisLabelsFontColor)//yAxis Label font color
+                             .fontSizeSet(AAFontSizeFormat(aaChartModel.yAxisLabelsFontSize))//yAxis Label font size
+                             .fontWeightSet(aaChartModel.yAxisLabelsFontWeight)//yAxis Label font weight
+                             )
+                   .formatSet(@"{value:.,0f}")//让y轴的值完整显示 而不是100000显示为100k
+                   )
+        .minSet(aaChartModel.yAxisMin)//设置 y 轴最小值,最小值等于零就不能显示负值了
+        .maxSet(aaChartModel.yAxisMax)//y轴最大值
+        .tickPositionsSet(aaChartModel.yAxisTickPositions)//自定义Y轴坐标
+        .allowDecimalsSet(aaChartModel.yAxisAllowDecimals)//是否允许显示小数
+        .plotLinesSet(aaChartModel.yAxisPlotLines) //标示线设置
+        .reversedSet(aaChartModel.yAxisReversed)
+        .gridLineWidthSet(aaChartModel.yAxisGridLineWidth)//y轴网格线宽度
+        .titleSet(AAAxisTitle.new.textSet(aaChartModel.yAxisTitle))//y 轴标题
+        .lineWidthSet(aaChartModel.yAxisLineWidth)//设置 y轴轴线的宽度,为0即是隐藏 y轴轴线
+        .visibleSet(aaChartModel.yAxisVisible)
+        .tickIntervalSet(aaChartModel.yAxisTickInterval);
+        
+        if (aaChartModel.yAxisCrosshairWidth > 0) {
+            aaYAxis.crosshairSet(AACrosshair.new
+                                 .widthSet(aaChartModel.yAxisCrosshairWidth)
+                                 .colorSet(aaChartModel.yAxisCrosshairColor)
+                                 .dashStyleSet(aaChartModel.yAxisCrosshairDashStyleType)
+                                 );
+        }
+        
+        aaOptions.xAxis = aaXAxis;
+        aaOptions.yAxis = aaYAxis;
+    }
+}
 
 
 @end
