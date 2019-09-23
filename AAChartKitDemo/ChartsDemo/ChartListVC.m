3e4efa22002ede8c0e28399bb71d3acdf3f769c7
AnAn <2236368544@qq.com>
2019-09-23 18:35:36 +0800

    Beautify code layout

--- AAChartKitDemo/ChartsDemo/ChartListVC.m
+++ AAChartKitDemo/ChartsDemo/ChartListVC.m
@@ -51,32 +51,34 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.title = @"AAInfographics";
     
-    _chartTypeArr = @[AAChartTypeColumn,
-                      AAChartTypeBar,
-                      AAChartTypeLine,
-                      AAChartTypeSpline,
-                      AAChartTypeArea,
-                      AAChartTypeAreaspline,
-                      AAChartTypeColumn,
-                      AAChartTypeBar,
-                      AAChartTypeLine,
-                      AAChartTypeSpline,
-                      AAChartTypeArea,
-                      AAChartTypeAreaspline,
-                      AAChartTypeBubble,
-                      AAChartTypeColumn,
-                      AAChartTypeLine,
-                      AAChartTypeSpline,
-                      AAChartTypeArea,
-                      AAChartTypeAreaspline,
-                      AAChartTypeBubble,
-                      AAChartTypeColumn,
-                      AAChartTypeLine,
-                      AAChartTypeSpline,
-                      AAChartTypeArea,
-                      AAChartTypeAreaspline,
-                      AAChartTypeBubble,
-                      ];
+    _chartTypeArr =
+    @[
+      AAChartTypeColumn,
+      AAChartTypeBar,
+      AAChartTypeLine,
+      AAChartTypeSpline,
+      AAChartTypeArea,
+      AAChartTypeAreaspline,
+      AAChartTypeColumn,
+      AAChartTypeBar,
+      AAChartTypeLine,
+      AAChartTypeSpline,
+      AAChartTypeArea,
+      AAChartTypeAreaspline,
+      AAChartTypeBubble,
+      AAChartTypeColumn,
+      AAChartTypeLine,
+      AAChartTypeSpline,
+      AAChartTypeArea,
+      AAChartTypeAreaspline,
+      AAChartTypeBubble,
+      AAChartTypeColumn,
+      AAChartTypeLine,
+      AAChartTypeSpline,
+      AAChartTypeArea,
+      AAChartTypeAreaspline,
+      AAChartTypeBubble,
+      ];
     
     [self setUpBasicView];
 }
@@ -144,56 +146,57 @@
     } else if ([chartType isEqualToString:AAChartTypeArea] || [chartType isEqualToString:AAChartTypeLine]) {
         chartModel.stacking = AAChartStackingTypeFalse;
     } else if ([chartType isEqualToString:AAChartTypeBubble]) {
-        chartModel.series = @[
-                              AASeriesElement.new
-                              .nameSet(@"2017")
-                              .fillColorSet(@"rgba(255,255,255,0.33)")
-                              .dataSet(
-                                       @[
-                                         @[@97, @36, @79],
-                                         @[@94, @74, @60],
-                                         @[@68, @76, @58],
-                                         @[@64, @87, @56],
-                                         @[@68, @27, @73],
-                                         @[@74, @99, @42],
-                                         @[@7 , @93, @87],
-                                         @[@51, @69, @40],
-                                         @[@38, @23, @33],
-                                         @[@57, @86, @31]
-                                         ]),
-                              AASeriesElement.new
-                              .nameSet(@"2018")
-                              .fillColorSet(@"rgba(255,255,255,0.66)")
-                              .dataSet(
-                                       @[
-                                         @[@25, @10, @87],
-                                         @[@2 , @75, @59],
-                                         @[@11, @54, @8 ],
-                                         @[@86, @55, @93],
-                                         @[@5 , @3 , @58],
-                                         @[@90, @63, @44],
-                                         @[@91, @33, @17],
-                                         @[@97, @3 , @56],
-                                         @[@15, @67, @48],
-                                         @[@54, @25, @81]
-                                         ]),
-                              AASeriesElement.new
-                              .nameSet(@"2019")
-                              .fillColorSet(@"rgba(255,255,255,0.99)")
-                              .dataSet(
-                                       @[
-                                         @[@47, @47, @21],
-                                         @[@20, @12, @4 ],
-                                         @[@6 , @76, @91],
-                                         @[@38, @30, @60],
-                                         @[@57, @98, @64],
-                                         @[@61, @17, @80],
-                                         @[@83, @60, @13],
-                                         @[@67, @78, @75],
-                                         @[@64, @12, @10],
-                                         @[@30, @77, @82]
-                                         ]),
-                              ];
+        chartModel.series =
+        @[
+          AASeriesElement.new
+          .nameSet(@"2017")
+          .fillColorSet(@"rgba(255,255,255,0.33)")
+          .dataSet(
+                   @[
+                     @[@97, @36, @79],
+                     @[@94, @74, @60],
+                     @[@68, @76, @58],
+                     @[@64, @87, @56],
+                     @[@68, @27, @73],
+                     @[@74, @99, @42],
+                     @[@7 , @93, @87],
+                     @[@51, @69, @40],
+                     @[@38, @23, @33],
+                     @[@57, @86, @31]
+                     ]),
+          AASeriesElement.new
+          .nameSet(@"2018")
+          .fillColorSet(@"rgba(255,255,255,0.66)")
+          .dataSet(
+                   @[
+                     @[@25, @10, @87],
+                     @[@2 , @75, @59],
+                     @[@11, @54, @8 ],
+                     @[@86, @55, @93],
+                     @[@5 , @3 , @58],
+                     @[@90, @63, @44],
+                     @[@91, @33, @17],
+                     @[@97, @3 , @56],
+                     @[@15, @67, @48],
+                     @[@54, @25, @81]
+                     ]),
+          AASeriesElement.new
+          .nameSet(@"2019")
+          .fillColorSet(@"rgba(255,255,255,0.99)")
+          .dataSet(
+                   @[
+                     @[@47, @47, @21],
+                     @[@20, @12, @4 ],
+                     @[@6 , @76, @91],
+                     @[@38, @30, @60],
+                     @[@57, @98, @64],
+                     @[@61, @17, @80],
+                     @[@83, @60, @13],
+                     @[@67, @78, @75],
+                     @[@64, @12, @10],
+                     @[@30, @77, @82]
+                     ]),
+          ];
     }
     if (indexPath.row > 6 && indexPath.row <= 12) {
         chartModel.stacking = AAChartStackingTypePercent;
