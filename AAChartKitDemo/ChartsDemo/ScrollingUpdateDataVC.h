227d7021317ad11e81d53ad944fc67b10b695aa9
AnAn <2236368544@qq.com>
2019-11-10 01:25:39 +0800

    Add ScrollingUpdateDataVC

---
+++ AAChartKitDemo/ChartsDemo/ScrollingUpdateDataVC.h
@@ -0,0 +1,17 @@
+//
+//  ScrollingUpdateDataVC.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2019/11/9.
+//  Copyright © 2019 Danny boy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScrollingUpdateDataVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AAChartKitDemo/ChartsDemo/ScrollingUpdateDataVC.m
@@ -0,0 +1,32 @@
+//
+//  ScrollingUpdateDataVC.m
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2019/11/9.
+//  Copyright © 2019 Danny boy. All rights reserved.
+//
+
+#import "ScrollingUpdateDataVC.h"
+
+@interface ScrollingUpdateDataVC ()
+
+@end
+
+@implementation ScrollingUpdateDataVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
