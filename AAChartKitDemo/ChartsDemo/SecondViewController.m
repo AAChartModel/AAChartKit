2e038a93d1657bbc553523f4ec9d2b69bc2f7136
AnAn <2236368544@qq.com>
2020-01-12 14:43:50 +0800

    Format code

--- AAChartKitDemo/ChartsDemo/SecondViewController.m
+++ AAChartKitDemo/ChartsDemo/SecondViewController.m
@@ -114,20 +114,20 @@
     .yAxisGridLineWidthSet(@0)//y轴横向分割线宽度为0(即是隐藏分割线)
     .touchEventEnabledSet(true)//支持用户点击事件
     .seriesSet(@[
-                 AASeriesElement.new
-                 .nameSet(@"2017")
-                 .dataSet(@[@7.0, @6.9, @9.5, @14.5, @18.2, @21.5, @25.2, @26.5, @23.3, @18.3, @13.9, @9.6]),
-                 AASeriesElement.new
-                 .nameSet(@"2018")
-                 .dataSet(@[@0.2, @0.8, @5.7, @11.3, @17.0, @22.0, @24.8, @24.1, @20.1, @14.1, @8.6, @2.5]),
-                 AASeriesElement.new
-                 .nameSet(@"2019")
-                 .dataSet(@[@0.9, @0.6, @3.5, @8.4, @13.5, @17.0, @18.6, @17.9, @14.3, @9.0, @3.9, @1.0]),
-                 AASeriesElement.new
-                 .nameSet(@"2020")
-                 .dataSet(@[@3.9, @4.2, @5.7, @8.5, @11.9, @15.2, @17.0, @16.6, @14.2, @10.3, @6.6, @4.8]),
-                 ]
-               );
+        AASeriesElement.new
+        .nameSet(@"2017")
+        .dataSet(@[@7.0, @6.9, @9.5, @14.5, @18.2, @21.5, @25.2, @26.5, @23.3, @18.3, @13.9, @9.6]),
+        AASeriesElement.new
+        .nameSet(@"2018")
+        .dataSet(@[@0.2, @0.8, @5.7, @11.3, @17.0, @22.0, @24.8, @24.1, @20.1, @14.1, @8.6, @2.5]),
+        AASeriesElement.new
+        .nameSet(@"2019")
+        .dataSet(@[@0.9, @0.6, @3.5, @8.4, @13.5, @17.0, @18.6, @17.9, @14.3, @9.0, @3.9, @1.0]),
+        AASeriesElement.new
+        .nameSet(@"2020")
+        .dataSet(@[@3.9, @4.2, @5.7, @8.5, @11.9, @15.2, @17.0, @16.6, @14.2, @10.3, @6.6, @4.8]),
+    ]);
+    
     [self configureTheStyleForDifferentTypeChart];//为不同类型图表设置样式
     
     /*配置 Y 轴标注线,解开注释,即可查看添加标注线之后的图表效果(NOTE:必须设置 Y 轴可见)*/
@@ -150,37 +150,36 @@
     .yAxisAllowDecimalsSet(NO)//是否允许Y轴坐标值小数
     .yAxisTickPositionsSet(@[@(0),@(25),@(50),@(75),@(100)])//指定y轴坐标
     .yAxisPlotLinesSet(@[
-                         AAPlotLinesElement.new
-                         .colorSet(@"#F05353")//颜色值(16进制)
-                         .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)//样式：Dash,Dot,Solid等,默认Solid
-                         .widthSet(@(1)) //标示线粗细
-                         .valueSet(@(20)) //所在位置
-                         .zIndexSet(@(1)) //层叠,标示线在图表中显示的层叠级别，值越大，显示越向前
-                         .labelSet(AALabel.new
-                                   .textSet(@"标示线1")
-                                   .styleSet(AAStyle.new
-                                             .colorSet(@"#ff0000")))
-                         ,AAPlotLinesElement.new
-                         .colorSet(@"#33BDFD")
-                         .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)
-                         .widthSet(@(1))
-                         .valueSet(@(40))
-                         .labelSet(AALabel.new
-                                   .textSet(@"标示线2")
-                                   .styleSet(AAStyle.new
-                                             .colorSet(@"#00ff00")))
-                         ,AAPlotLinesElement.new
-                         .colorSet(@"#ADFF2F")
-                         .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)
-                         .widthSet(@(1))
-                         .valueSet(@(60))
-                         .labelSet(AALabel.new
-                                   .textSet(@"标示线3")
-                                   .styleSet(AAStyle.new
-                                             .colorSet(@"#0000ff")))
-
-                         ]
-                   );
+        AAPlotLinesElement.new
+        .colorSet(@"#F05353")//颜色值(16进制)
+        .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)//样式：Dash,Dot,Solid等,默认Solid
+        .widthSet(@(1)) //标示线粗细
+        .valueSet(@(20)) //所在位置
+        .zIndexSet(@(1)) //层叠,标示线在图表中显示的层叠级别，值越大，显示越向前
+        .labelSet(AALabel.new
+                  .textSet(@"标示线1")
+                  .styleSet(AAStyle.new
+                            .colorSet(@"#ff0000")))
+        ,AAPlotLinesElement.new
+        .colorSet(@"#33BDFD")
+        .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)
+        .widthSet(@(1))
+        .valueSet(@(40))
+        .labelSet(AALabel.new
+                  .textSet(@"标示线2")
+                  .styleSet(AAStyle.new
+                            .colorSet(@"#00ff00")))
+        ,AAPlotLinesElement.new
+        .colorSet(@"#ADFF2F")
+        .dashStyleSet(AAChartLineDashStyleTypeLongDashDot)
+        .widthSet(@(1))
+        .valueSet(@(60))
+        .labelSet(AALabel.new
+                  .textSet(@"标示线3")
+                  .styleSet(AAStyle.new
+                            .colorSet(@"#0000ff")))
+        
+    ]);
 }
 
 - (void)configureTheStyleForDifferentTypeChart {
