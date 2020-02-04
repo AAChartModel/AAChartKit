1c39b1dd5cc545480ddaff5d73852fc1d2ca1a75
AnAn <2236368544@qq.com>
2020-02-04 22:46:35 +0800

    Add new drawable chart

---
+++ AAChartKitDemo/ChartsDemo/DrawableChartVC.h
@@ -0,0 +1,17 @@
+//
+//  DragableChartVC.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/2/4.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DrawableChartVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AAChartKitDemo/ChartsDemo/DrawableChartVC.m
@@ -0,0 +1,32 @@
+//
+//  DragableChartVC.m
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/2/4.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//
+
+#import "DrawableChartVC.h"
+
+@interface DrawableChartVC ()
+
+@end
+
+@implementation DrawableChartVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
