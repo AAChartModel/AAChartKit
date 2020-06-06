4364b9c66ae512cb2608e3d5a35d3e80aaf8e5cb
An An <2236368544@qq.com>
2020-06-06 19:28:05 +0800

    Add new VC files

---
+++ AAChartKitDemo/ChartsDemo/AdvancedChartOptionsUpdatingFunctionVC.m
@@ -0,0 +1,56 @@
+//
+//  AdvancedChartOptionsUpdatingFunctionVC.m
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/6/6.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//*************** ...... SOURCE CODE ...... ***************
+//***...................................................***
+//*** https://github.com/AAChartModel/AAChartKit        ***
+//*** https://github.com/AAChartModel/AAChartKit-Swift  ***
+//***...................................................***
+//*************** ...... SOURCE CODE ...... ***************
+
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ * 🌕 🌖 🌗 🌘  ❀❀❀   WARM TIPS!!!   ❀❀❀ 🌑 🌒 🌓 🌔
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/7842508/codeforu
+ * JianShu       : https://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
+
+#import "AdvancedChartOptionsUpdatingFunctionVC.h"
+
+@interface AdvancedChartOptionsUpdatingFunctionVC ()
+
+@end
+
+@implementation AdvancedChartOptionsUpdatingFunctionVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
