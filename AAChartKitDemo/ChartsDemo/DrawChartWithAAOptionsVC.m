f4aec009ff540e468b6a85c9d34fa03375856913
An An <2236368544@qq.com>
2020-06-17 18:03:28 +0800

    Format code

--- AAChartKitDemo/ChartsDemo/DrawChartWithAAOptionsVC.m
+++ AAChartKitDemo/ChartsDemo/DrawChartWithAAOptionsVC.m
@@ -1669,13 +1669,14 @@
     .yAxisTitleSet(@"")
     .categoriesSet(@[@"一月", @"二月", @"三月", @"四月", @"五月", @"六月", @"七月", @"八月", @"九月", @"十月", @"十一月", @"十二月"])
     .markerRadiusSet(@0)
-    .seriesSet(@[AASeriesElement.new
-                 .nameSet(@"Berlin Hot")
-                 .colorSet(AAColor.whiteColor)
-                 .lineWidthSet(@7)
-                 .fillColorSet((id)fillColorGradientColor)
-                 .dataSet(@[@7.0, @6.9, @2.5, @14.5, @18.2, @21.5, @5.2, @26.5, @23.3, @45.3, @13.9, @9.6]),
-    ]);
+    .seriesSet(@[
+        AASeriesElement.new
+        .nameSet(@"Berlin Hot")
+        .colorSet(AAColor.whiteColor)
+        .lineWidthSet(@7)
+        .fillColorSet((id)fillColorGradientColor)
+        .dataSet(@[@7.0, @6.9, @2.5, @14.5, @18.2, @21.5, @5.2, @26.5, @23.3, @45.3, @13.9, @9.6]),
+               ]);
     
     AAOptions *aaOptions = [AAOptionsConstructor configureChartOptionsWithAAChartModel:aaChartModel];
     
