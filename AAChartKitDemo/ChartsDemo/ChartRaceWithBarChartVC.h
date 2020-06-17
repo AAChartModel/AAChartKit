20d3ef8d8e7832108f965d2e87b79a62cdf6f3ae
An An <2236368544@qq.com>
2020-06-17 16:12:25 +0800

    Add new ChartRaceWithBarChartVC

---
+++ AAChartKitDemo/ChartsDemo/ChartRaceWithBarChartVC.h
@@ -0,0 +1,17 @@
+//
+//  ChartRaceWithBarChartVC.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/6/16.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChartRaceWithBarChartVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
