dafb7070ab8e9e06ca4f98ec54f01b03de185e48
An An <2236368544@qq.com>
2020-06-22 14:25:09 +0800

    Fix #793 #277 #485 #847 => Add AAWeakProxy for AAChartView to dealloc successfully

--- AAChartKitLib/AAChartCreator/AAChartView.m
+++ AAChartKitLib/AAChartCreator/AAChartView.m
@@ -33,6 +33,85 @@
 #import "AAChartView.h"
 #import "AAJSStringPurer.h"
 
+@interface AAWeakProxy : NSProxy
+
+@property (nonatomic, weak, readonly, nullable) id target;
+
+- (nonnull instancetype)initWithTarget:(nonnull id)target;
++ (nonnull instancetype)proxyWithTarget:(nonnull id)target;
+
+@end
+
+@implementation AAWeakProxy
+
+- (instancetype)initWithTarget:(id)target {
+    _target = target;
+    return self;
+}
+
++ (instancetype)proxyWithTarget:(id)target {
+    return [[AAWeakProxy alloc] initWithTarget:target];
+}
+
+- (id)forwardingTargetForSelector:(SEL)selector {
+    return _target;
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation {
+    void *null = NULL;
+    [invocation setReturnValue:&null];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    return [NSObject instanceMethodSignatureForSelector:@selector(init)];
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    return [_target respondsToSelector:aSelector];
+}
+
+- (BOOL)isEqual:(id)object {
+    return [_target isEqual:object];
+}
+
+- (NSUInteger)hash {
+    return [_target hash];
+}
+
+- (Class)superclass {
+    return [_target superclass];
+}
+
+- (Class)class {
+    return [_target class];
+}
+
+- (BOOL)isKindOfClass:(Class)aClass {
+    return [_target isKindOfClass:aClass];
+}
+
+- (BOOL)isMemberOfClass:(Class)aClass {
+    return [_target isMemberOfClass:aClass];
+}
+
+- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
+    return [_target conformsToProtocol:aProtocol];
+}
+
+- (BOOL)isProxy {
+    return YES;
+}
+
+- (NSString *)description {
+    return [_target description];
+}
+
+- (NSString *)debugDescription {
+    return [_target debugDescription];
+}
+
+@end
+
 @implementation AAMoveOverEventMessageModel
 
 @end
@@ -67,7 +146,8 @@ WKScriptMessageHandler
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     config.userContentController = _userContentController;
     self = [super initWithFrame:frame configuration:config];
-    [config.userContentController addScriptMessageHandler:self
+    AAWeakProxy *proxy = [AAWeakProxy proxyWithTarget:self];
+    [config.userContentController addScriptMessageHandler:(id<WKScriptMessageHandler>)proxy
                                                      name:kUserContentMessageNameMouseOver];
     if (self) {
         self.UIDelegate = self;
