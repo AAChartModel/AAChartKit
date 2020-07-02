2015314fa04b042d211bb66d8e6b56a1e7363748
An An <2236368544@qq.com>
2020-07-03 00:36:41 +0800

    Delete useless code `fillOpacity` for line chart

--- AAChartKitDemo/ChartsDemo/CustomStyleChartVC.m
+++ AAChartKitDemo/ChartsDemo/CustomStyleChartVC.m
@@ -1230,7 +1230,6 @@
     .seriesSet(@[
         AASeriesElement.new
         .nameSet(@"2017")
-        .fillOpacitySet(@1.0)
         .stepSet(@(true))
         .dataLabelsSet(AADataLabels.new
                        .styleSet(AAStyle.new
@@ -1240,7 +1239,6 @@
         .dataSet(@[@2.10, @2.54, @2.78, @3.62, @4.41, @4.09, @3.83, @4.47, @4.20, @3.94, @3.80, @3.58, @3.19, @4.30, @3.69, @3.52, @3.02, @3.30]),
         AASeriesElement.new
         .nameSet(@"2018")
-        .fillOpacitySet(@1.0)
         .stepSet(@(true))
         .dataLabelsSet(AADataLabels.new
                        .styleSet(AAStyle.new
@@ -1250,7 +1248,6 @@
         .dataSet(@[@1.56, @1.91, @2.45, @3.87, @3.24, @4.90, @4.61, @4.10, @4.17, @3.85, @4.17, @3.46, @3.46, @3.55, @3.50, @4.13, @2.58, @2.28]),
         AASeriesElement.new
         .nameSet(@"2019")
-        .fillOpacitySet(@1.0)
         .stepSet(@(true))
         .dataLabelsSet(AADataLabels.new
                        .styleSet(AAStyle.new
@@ -1260,7 +1257,6 @@
         .dataSet(@[@1.16, @1.67, @2.64, @2.86, @3.00, @3.21, @4.14, @4.07, @3.68, @3.11, @3.41, @3.25, @3.32, @3.07, @3.92, @3.05, @2.18, @3.24]),
         AASeriesElement.new
         .nameSet(@"2020")
-        .fillOpacitySet(@1.0)
         .stepSet(@(true))
         .dataLabelsSet(AADataLabels.new
                        .styleSet(AAStyle.new
