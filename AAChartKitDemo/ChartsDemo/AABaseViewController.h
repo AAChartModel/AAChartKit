01a79fb2d53eb252372c9e2d1f422b16bcd35e56
An An <2236368544@qq.com>
2020-07-05 21:46:56 +0800

    Add AABaseViewController

---
+++ AAChartKitDemo/ChartsDemo/AABaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  AABaseViewController.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/7/5.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AABaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AAChartKitDemo/ChartsDemo/AABaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  AABaseViewController.m
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2020/7/5.
+//  Copyright © 2020 Danny boy. All rights reserved.
+//
+
+#import "AABaseViewController.h"
+
+@interface AABaseViewController ()
+
+@end
+
+@implementation AABaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
