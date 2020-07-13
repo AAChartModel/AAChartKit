895260578f02d732284bed2c268fb9296d627e07
An An <2236368544@qq.com>
2020-07-13 13:52:12 +0800

    Remove redundant code for AAChartView

--- AAChartKitLib/AAChartCreator/AAChartView.m
+++ AAChartKitLib/AAChartCreator/AAChartView.m
@@ -37,7 +37,6 @@
 
 @property (nonatomic, weak, readonly, nullable) id target;
 
-- (nonnull instancetype)initWithTarget:(nonnull id)target;
 + (nonnull instancetype)proxyWithTarget:(nonnull id)target;
 
 @end
@@ -68,46 +67,6 @@
 
 - (BOOL)respondsToSelector:(SEL)aSelector {
     return [_target respondsToSelector:aSelector];
-}
-
-- (BOOL)isEqual:(id)object {
-    return [_target isEqual:object];
-}
-
-- (NSUInteger)hash {
-    return [_target hash];
-}
-
-- (Class)superclass {
-    return [_target superclass];
-}
-
-- (Class)class {
-    return [_target class];
-}
-
-- (BOOL)isKindOfClass:(Class)aClass {
-    return [_target isKindOfClass:aClass];
-}
-
-- (BOOL)isMemberOfClass:(Class)aClass {
-    return [_target isMemberOfClass:aClass];
-}
-
-- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
-    return [_target conformsToProtocol:aProtocol];
-}
-
-- (BOOL)isProxy {
-    return YES;
-}
-
-- (NSString *)description {
-    return [_target description];
-}
-
-- (NSString *)debugDescription {
-    return [_target debugDescription];
 }
 
 @end
