7934c218f688f261ab1de4df77d87dc52cd0395b
An An <2236368544@qq.com>
2020-07-15 15:12:18 +0800

    Fix #937 => Optimize customLineChartOriginalPointPositionByConfiguringXAxisFormatterAndTooltipFormatter

--- AAChartKitDemo/ChartsDemo/JSFormatterFunctionVC.m
+++ AAChartKitDemo/ChartsDemo/JSFormatterFunctionVC.m
@@ -983,6 +983,7 @@
 }
 
 //https://github.com/AAChartModel/AAChartKit/issues/577
+//https://github.com/AAChartModel/AAChartKit/issues/937
 - (AAOptions *)customLineChartOriginalPointPositionByConfiguringXAxisFormatterAndTooltipFormatter {
     AAChartModel *aaChartModel = AAChartModel.new
     .chartTypeSet(AAChartTypeLine)//图表类型
@@ -996,16 +997,16 @@
     .seriesSet(@[
         AASeriesElement.new
         .nameSet(@"2017")
-        .dataSet(@[@7.0, @6.9, @9.5, @14.5, @18.2, @21.5, @25.2, @26.5, @23.3, @18.3, @13.9, @9.6]),
+        .dataSet(@[@7.0, @6.9, @9.5, @14.5, @18.2, @21.5,]),
         AASeriesElement.new
         .nameSet(@"2018")
-        .dataSet(@[@0.2, @0.8, @5.7, @11.3, @17.0, @22.0, @24.8, @24.1, @20.1, @14.1, @8.6, @2.5]),
+        .dataSet(@[@0.2, @0.8, @5.7, @11.3, @17.0, @22.0, ]),
         AASeriesElement.new
         .nameSet(@"2019")
-        .dataSet(@[@0.9, @0.6, @3.5, @8.4, @13.5, @17.0, @18.6, @17.9, @14.3, @9.0, @3.9, @1.0]),
+        .dataSet(@[@0.9, @0.6, @3.5, @8.4, @13.5, @17.0, ]),
         AASeriesElement.new
         .nameSet(@"2020")
-        .dataSet(@[@3.9, @4.2, @5.7, @8.5, @11.9, @15.2, @17.0, @16.6, @14.2, @10.3, @6.6, @4.8]),
+        .dataSet(@[@3.9, @4.2, @5.7, @8.5, @11.9, @15.2, ]),
     ]);
     
     AAOptions *aaOptions = [AAOptionsConstructor configureChartOptionsWithAAChartModel:aaChartModel];
