a4b326d9945f9c00be03a492c3892ef9baaabddb
An An <2236368544@qq.com>
2022-06-16 13:57:48 +0800

    Remove useless code for column chart

--- AAChartKitDemo/ChartsDemo/CustomStyleChartVC.m
+++ AAChartKitDemo/ChartsDemo/CustomStyleChartVC.m
@@ -677,7 +677,6 @@
     .seriesSet(@[
         AASeriesElement.new
         .nameSet(@"Virtual Data")
-        .lineWidthSet(@6)
         .dataSet(@[@7.0, @6.9, @2.5, @14.5, @18.2, singleSpecialData, @5.2, @26.5, @23.3, @26.5, @13.9, @9.6])
         .colorSet((id)AAGradientColor.oceanBlueColor)
                ]);
@@ -1850,7 +1849,6 @@
     .seriesSet(@[
         AASeriesElement.new
         .nameSet(@"虚构数据")
-        .lineWidthSet(@6)
         .dataSet(newDataArr)
                ]);
 }
