36aa82ac9ed68e9fcd80946f234dbc9af4009c92
An An <2236368544@qq.com>
2022-08-26 21:49:39 +0800

    Add JSFunctionForAAChartEventsVC

---
+++ AAChartKitDemo/ChartsDemo/JSFunctionForAAChartEventsVC.h
@@ -0,0 +1,17 @@
+//
+//  JSFunctionForAAChartEventsVC.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2022/8/26.
+//  Copyright © 2022 An An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSFunctionForAAChartEventsVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
