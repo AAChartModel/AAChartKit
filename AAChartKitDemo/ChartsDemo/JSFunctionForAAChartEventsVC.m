104b80eead9e0a6bcb8097c6bbb06dda9fa77085
An An <2236368544@qq.com>
2022-08-28 21:43:24 +0800

    Update JSFunctionForAAChartEventsVC.m

--- AAChartKitDemo/ChartsDemo/JSFunctionForAAChartEventsVC.m
+++ AAChartKitDemo/ChartsDemo/JSFunctionForAAChartEventsVC.m
@@ -758,6 +758,207 @@
 
 }
 
+//https://github.com/AAChartModel/AAChartKit-Swift/issues/394
+//https://www.highcharts.com/forum/viewtopic.php?t=44985
+- (AAOptions *)configureBlinkMarkerChart {
+    NSArray *dataArr = @[@7.0, @6.9, @2.5, @14.5, @18.2, @21.5, @5.2, @26.5, @23.3, @45.3, @13.9, @9.6];
+    NSInteger selectedIndex = dataArr.count - 1;
+    return AAOptions.new
+            .chartSet(AAChart.new
+                    .eventsSet(AAChartEvents.new
+                            .loadSet([NSString stringWithFormat:@AAJSFunc(function() {
+                                const chart = this;
+                                const point = chart.series[0].points[%ld];
+                                let big = true;
+                                setInterval(() => {
+                                        big = !big;
+                                        point.update({
+                                            marker: {
+                                            radius: big ? 5 : 15
+                                        }
+                                        })
+                                }, 500);
+                            }),selectedIndex])
+                               ))
+            .seriesSet(@[
+                    AASeriesElement.new
+                            .dataSet(dataArr)
+                            .markerSet(AAMarker.new
+                                    .lineColorSet(AAColor.redColor)
+                                    .lineWidthSet(@3)
+                                    .radiusSet(@10))
+            ]);
+}
+
+//https://github.com/AAChartModel/AAChartKit-Swift/issues/394
+//https://www.highcharts.com/forum/viewtopic.php?t=44985
+- (AAOptions *)configureSpecialStyleMarkerOfSingleDataElementChartWithBlinkEffect {
+    NSArray *stopsArr = @[
+            @[@0.00, @"#febc0f"],//颜色字符串设置支持十六进制类型和 rgba 类型
+            @[@0.25, @"#FF14d4"],
+            @[@0.50, @"#0bf8f5"],
+            @[@0.75, @"#F33c52"],
+            @[@1.00, @"#1904dd"],
+    ];
+
+    NSDictionary *gradientColorDic1 =
+            [AAGradientColor gradientColorWithDirection:AALinearGradientDirectionToRight
+                                             stopsArray:stopsArr];
+
+    AADataElement *singleSpecialData = AADataElement.new
+            .markerSet(AAMarker.new
+                    .radiusSet(@8)//曲线连接点半径
+                    .symbolSet(AAChartSymbolTypeCircle)//曲线点类型："circle", "square", "diamond", "triangle","triangle-down"，默认是"circle"
+                    .fillColorSet(AAColor.whiteColor)//点的填充色(用来设置折线连接点的填充色)
+                    .lineWidthSet(@5)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
+                            //外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色)
+                    .lineColorSet(AAColor.redColor)
+            )
+            .ySet(@26.5);
+
+    AAChartModel *aaChartModel = AAChartModel.new
+            .chartTypeSet(AAChartTypeSpline)
+            .backgroundColorSet(@"#4b2b7f")
+            .dataLabelsEnabledSet(false)//是否显示值
+            .tooltipEnabledSet(true)
+            .markerRadiusSet(@0)
+            .xAxisVisibleSet(false)
+            .yAxisVisibleSet(false)
+            .seriesSet(@[
+                    AASeriesElement.new
+                            .nameSet(@"Virtual Data")
+                            .lineWidthSet(@6)
+                            .dataSet(@[@7.0, @6.9, @2.5, @14.5, @18.2, singleSpecialData, @5.2, @26.5, @23.3, @45.3, @13.9, @9.6])
+                            .colorSet((id)gradientColorDic1)
+            ]);
+    NSInteger selectedIndex = 11;
+    AAOptions *aaOptions = aaChartModel.aa_toAAOptions;
+    aaOptions.chart.events = AAChartEvents.new
+           .loadSet([NSString stringWithFormat:@AAJSFunc(
+                     function () {
+                          const chart = this;
+                          const point = chart.series[0].points[%ld];
+                          let big = true;
+                          setInterval(() => {
+                                 big = !big;
+                                 point.update({
+                                      marker: {
+                                        radius: big ? 5 : 55,
+                                        fillColor: big ? "white": "red",
+                                        lineWidth: big ? 5 : 15,
+                                        lineColor: big ? "green": "yellow",
+                                      }
+                                 })
+                          }, 500)
+                     }
+                     ), (long)selectedIndex]);
+    return aaOptions;
+}
+
+//https://github.com/AAChartModel/AAChartKit-Swift/issues/394
+//https://echarts.apache.org/examples/zh/editor.html?c=scatter-effect
+- (AAOptions *)configureScatterChartWithBlinkEffect {
+    AAChartModel *aaChartModel = AAChartModel.new
+            .chartTypeSet(AAChartTypeScatter)
+            .titleSet(@"按性别划分的身高体重分布图")
+            .yAxisTitleSet(@"千克(kg)")
+            .markerRadiusSet(@9)
+            .yAxisGridLineStyleSet([AALineStyle styleWithWidth:@0])
+            .colorsThemeSet(@[@"#c3b1fb",@"#e70b2d",])
+            .markerSymbolStyleSet(AAChartSymbolStyleTypeInnerBlank)
+            .markerSymbolSet(AAChartSymbolTypeCircle)
+            .markerRadiusSet(@9)
+            .backgroundColorSet(AAColor.blackColor)
+            .seriesSet(@[
+                    AASeriesElement.new
+                            .nameSet(@"Blink Scatter")
+                            .colorSet(AAColor.greenColor)
+                            .dataSet(@[
+                                    @[@172.7, @105.2],
+                                    @[@153.4, @42]
+                            ])
+                                    ,
+                    AASeriesElement.new
+                            .nameSet(@"女")
+                            .dataSet(@[
+                                    @[@174.0, @65.6], @[@175.3, @71.8], @[@193.5, @80.7], @[@186.5, @72.6], @[@187.2, @78.8],
+                                    @[@181.5, @74.8], @[@184.0, @86.4], @[@184.5, @78.4], @[@175.0, @62.0], @[@184.0, @81.6],
+                                    @[@180.0, @76.6], @[@177.8, @83.6], @[@192.0, @90.0], @[@176.0, @74.6], @[@174.0, @71.0],
+                                    @[@184.0, @79.6], @[@192.7, @93.8], @[@171.5, @70.0], @[@173.0, @72.4], @[@176.0, @85.9],
+                                    @[@176.0, @78.8], @[@180.5, @77.8], @[@172.7, @66.2], @[@176.0, @86.4], @[@173.5, @81.8],
+                                    @[@178.0, @89.6], @[@180.3, @82.8], @[@180.3, @76.4], @[@164.5, @63.2], @[@173.0, @60.9],
+                                    @[@183.5, @74.8], @[@175.5, @70.0], @[@188.0, @72.4], @[@189.2, @84.1], @[@172.8, @69.1],
+                                    @[@170.0, @59.5], @[@182.0, @67.2], @[@170.0, @61.3], @[@177.8, @68.6], @[@184.2, @80.1],
+                                    @[@186.7, @87.8], @[@171.4, @84.7], @[@172.7, @73.4], @[@175.3, @72.1], @[@180.3, @82.6],
+                                    @[@182.9, @88.7], @[@188.0, @84.1], @[@177.2, @94.1], @[@172.1, @74.9], @[@167.0, @59.1],
+                                    @[@169.5, @75.6], @[@174.0, @86.2], @[@172.7, @75.3], @[@182.2, @87.1], @[@164.1, @55.2],
+                                    @[@163.0, @57.0], @[@171.5, @61.4], @[@184.2, @76.8], @[@174.0, @86.8], @[@174.0, @72.2],
+                                    @[@177.0, @71.6], @[@186.0, @84.8], @[@167.0, @68.2], @[@171.8, @66.1], @[@182.0, @72.0],
+                                    @[@167.0, @64.6], @[@177.8, @74.8], @[@164.5, @70.0], @[@192.0, @92.2], @[@175.5, @63.2],
+                                    @[@171.2, @79.1], @[@181.6, @78.9], @[@167.4, @67.7], @[@181.1, @66.0], @[@177.0, @68.2],
+                                    @[@174.5, @63.9], @[@177.5, @72.0], @[@170.5, @56.8], @[@182.4, @74.5], @[@197.1, @90.9],
+                                    @[@180.1, @93.0], @[@175.5, @80.9], @[@180.6, @72.7], @[@184.4, @68.0], @[@175.5, @70.9],
+                                    @[@180.6, @72.5], @[@177.0, @72.5], @[@177.1, @83.4], @[@181.6, @75.5], @[@176.5, @73.0],
+                                    @[@175.0, @70.2], @[@174.0, @73.4], @[@165.1, @70.5], @[@177.0, @68.9], @[@192.0, @99.9],
+                                    @[@176.5, @68.4], @[@169.4, @65.9], @[@182.1, @75.7], @[@179.8, @84.5], @[@175.3, @87.7],
+                                    @[@184.9, @86.4], @[@177.3, @73.2], @[@167.4, @53.9], @[@178.1, @72.0], @[@168.9, @55.5],
+                                    @[@157.2, @58.4], @[@180.3, @83.2], @[@170.2, @72.7], @[@177.8, @64.1], @[@172.7, @72.3],
+                                    @[@165.1, @65.0], @[@186.7, @86.4], @[@165.1, @65.0], @[@174.0, @88.6], @[@175.3, @84.1],
+                                    @[@185.4, @66.8], @[@177.8, @75.5], @[@180.3, @93.2], @[@180.3, @82.7], @[@177.8, @58.0],
+                                    @[@177.8, @79.5], @[@177.8, @78.6], @[@177.8, @71.8], @[@177.8, @99.0], @[@163.8, @72.2],
+                                    @[@188.0, @83.6], @[@198.1, @85.5], @[@175.3, @90.9], @[@166.4, @85.9], @[@190.5, @89.1],
+                                    @[@166.4, @75.0], @[@177.8, @77.7], @[@179.7, @86.4], @[@172.7, @90.9], @[@190.5, @73.6],
+                                    @[@185.4, @76.4], @[@168.9, @69.1], @[@167.6, @84.5], @[@175.3, @64.5], @[@170.2, @69.1],
+                                    @[@190.5, @96.6], @[@177.8, @86.4], @[@190.5, @80.9], @[@177.8, @87.7], @[@184.2, @94.5],
+                                    @[@176.5, @80.2], @[@177.8, @72.0], @[@180.3, @71.4], @[@171.4, @72.7], @[@172.7, @84.1],
+                                    @[@172.7, @76.8], @[@177.8, @63.6], @[@177.8, @80.9], @[@182.9, @80.9], @[@170.2, @85.5],
+                                    @[@167.6, @68.6], @[@175.3, @67.7], @[@165.1, @66.4], @[@185.4, @99.9], @[@181.6, @70.5],
+                                    @[@172.7, @95.9], @[@190.5, @84.1], @[@179.1, @87.3], @[@175.3, @71.8], @[@170.2, @65.9],
+                                    @[@193.0, @95.9], @[@171.4, @91.4], @[@177.8, @81.8], @[@177.8, @96.8], @[@167.6, @69.1],
+                                    @[@167.6, @82.7], @[@180.3, @75.5], @[@182.9, @79.5], @[@176.5, @73.6], @[@186.7, @91.8],
+                                    @[@188.0, @84.1], @[@188.0, @85.9], @[@177.8, @81.8], @[@174.0, @82.5], @[@177.8, @80.5],
+                                    @[@171.4, @70.0], @[@185.4, @81.8], @[@185.4, @84.1], @[@188.0, @90.5], @[@188.0, @91.4],
+                                    @[@182.9, @89.1], @[@176.5, @85.0], @[@175.3, @69.1], @[@175.3, @73.6], @[@188.0, @80.5],
+                                    @[@188.0, @82.7], @[@175.3, @86.4], @[@170.5, @67.7], @[@179.1, @92.7], @[@177.8, @93.6],
+                                    @[@175.3, @70.9], @[@182.9, @75.0], @[@170.8, @93.2], @[@188.0, @93.2], @[@180.3, @77.7],
+                                    @[@177.8, @61.4], @[@185.4, @94.1], @[@168.9, @75.0], @[@185.4, @83.6], @[@180.3, @85.5],
+                                    @[@174.0, @73.9], @[@167.6, @66.8], @[@182.9, @87.3], @[@160.0, @72.3], @[@180.3, @88.6],
+                                    @[@167.6, @75.5], @[@186.7, @95.5], @[@175.3, @91.1], @[@175.3, @67.3], @[@175.9, @77.7],
+                                    @[@175.3, @81.8], @[@179.1, @75.5], @[@181.6, @84.5], @[@177.8, @76.6], @[@182.9, @85.0],
+                                    @[@177.8, @98.8], @[@184.2, @77.3], @[@179.1, @71.8], @[@176.5, @87.9], @[@188.0, @94.3],
+                                    @[@174.0, @70.9], @[@167.6, @64.5], @[@170.2, @77.3], @[@167.6, @72.3], @[@188.0, @87.3],
+                                    @[@174.0, @80.0], @[@176.5, @82.3], @[@180.3, @73.6], @[@167.6, @74.1], @[@188.0, @85.9],
+                                    @[@180.3, @73.2], @[@167.6, @76.3], @[@183.0, @65.9], @[@183.0, @90.9], @[@179.1, @89.1],
+                                    @[@170.2, @62.3], @[@177.8, @82.7], @[@179.1, @79.1], @[@190.5, @98.2], @[@177.8, @84.1],
+                                    @[@180.3, @83.2], @[@180.3, @83.2]
+                            ])
+            ]);
+    AAOptions *aaOptions = aaChartModel.aa_toAAOptions;
+    aaOptions.chart.eventsSet(
+            AAChartEvents.new
+            .loadSet(@AAJSFunc(function() {
+                const chart = this;
+                const blinkScatterPointsArr = chart.series[0].points;
+                const range = blinkScatterPointsArr.length;
+                for (let i = 0; i < range; i++) {
+                    const point = blinkScatterPointsArr[i];
+                    let big = true;
+                    setInterval(() => {
+                            big = !big;
+                            point.update({
+                                marker: {
+                                radius: big ? 5 : 25,
+                                    fillColor: big ? "pink": "red",
+                                        lineWidth: big ? 5 : 15,
+                                        lineColor: big ? "gold": "yellow",
+                            }
+                            })
+                    }, 600)
+                }
+            })));
+    return aaOptions;
+}
+
 - (AAOptions *)densityChart {
     return AAOptions.new
         .chartSet(AAChart.new
