9d572a2c9d6dfc5ac444ae0e84f94c1cea98b123
An An <2236368544@qq.com>
2022-09-06 18:49:13 +0800

    Fix #1386 => Add tickPositions for AAXAxis

--- AAChartKitLib/AAOptionsModel/AAXAxis.h
+++ AAChartKitLib/AAOptionsModel/AAXAxis.h
@@ -80,6 +80,7 @@ AAPropStatementAndPropSetFuncStatement(copy,   AAXAxis, NSString *, tickmarkPlac
 AAPropStatementAndPropSetFuncStatement(strong, AAXAxis, NSNumber *, tickWidth) //坐标轴刻度线的宽度，设置为 0 时则不显示刻度线
 AAPropStatementAndPropSetFuncStatement(strong, AAXAxis, NSNumber *, tickLength)//坐标轴刻度线的长度。 默认是：10.
 AAPropStatementAndPropSetFuncStatement(copy,   AAXAxis, NSString *, tickPosition) //刻度线相对于轴线的位置，可用的值有 inside 和 outside，分别表示在轴线的内部和外部。 默认是：outside.
+AAPropStatementAndPropSetFuncStatement(strong, AAXAxis, NSArray  *, tickPositions) //自定义 x 轴坐标（如：[@(0), @(25), @(50), @(75) , (100)]）
 
 
 @end
--- AAChartKitLib/AAOptionsModel/AAXAxis.m
+++ AAChartKitLib/AAOptionsModel/AAXAxis.m
@@ -82,5 +82,6 @@ AAPropSetFuncImplementation(AAXAxis, NSString *, tickmarkPlacement) //本参数�
 AAPropSetFuncImplementation(AAXAxis, NSNumber *, tickWidth) //坐标轴刻度线的宽度，设置为 0 时则不显示刻度线
 AAPropSetFuncImplementation(AAXAxis, NSNumber *, tickLength)//坐标轴刻度线的长度。 默认是：10.
 AAPropSetFuncImplementation(AAXAxis, NSString *, tickPosition) //刻度线相对于轴线的位置，可用的值有 inside 和 outside，分别表示在轴线的内部和外部。 默认是：outside.
+AAPropSetFuncImplementation(AAXAxis, NSArray  *, tickPositions) //自定义 x 轴坐标（如：[@(0), @(25), @(50), @(75) , (100)]）
 
 @end
