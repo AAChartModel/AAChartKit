3244f1d68b57118ea7381a7cf9c502b75102bbff
An An <2236368544@qq.com>
2022-09-09 16:02:41 +0800

    Append Copyright content

--- AAChartKitDemo/ChartsDemo/JSFunctionForAAAxisVC.m
+++ AAChartKitDemo/ChartsDemo/JSFunctionForAAAxisVC.m
@@ -4,7 +4,33 @@
 //
 //  Created by AnAn on 2022/9/9.
 //  Copyright © 2022 An An. All rights reserved.
-//
+//*************** ............ SOURCE CODE ............ ***************
+//***.............................................................  ***
+//*** iOS     :https://github.com/AAChartModel/AAChartKit           ***
+//*** iOS     :https://github.com/AAChartModel/AAChartKit-Swift     ***
+//*** Android :https://github.com/AAChartModel/AAChartCore          ***
+//*** Android :https://github.com/AAChartModel/AAChartCore-Kotlin   ***
+//***...............................................................***
+//*************** ............ SOURCE CODE ............ ***************
+
+/*
+ 
+ * -------------------------------------------------------------------------------
+ *
+ * 🌕 🌖 🌗 🌘  ❀❀❀   WARM TIPS!!!   ❀❀❀ 🌑 🌒 🌓 🌔
+ *
+ * Please contact me on GitHub,if there are any problems encountered in use.
+ * GitHub Issues : https://github.com/AAChartModel/AAChartKit/issues
+ * -------------------------------------------------------------------------------
+ * And if you want to contribute for this project, please contact me as well
+ * GitHub        : https://github.com/AAChartModel
+ * StackOverflow : https://stackoverflow.com/users/12302132/codeforu
+ * JianShu       : https://www.jianshu.com/u/f1e6753d4254
+ * SegmentFault  : https://segmentfault.com/u/huanghunbieguan
+ *
+ * -------------------------------------------------------------------------------
+ 
+ */
 
 #import "JSFunctionForAAAxisVC.h"
 
