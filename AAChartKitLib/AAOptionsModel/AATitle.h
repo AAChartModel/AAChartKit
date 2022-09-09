001dd9197b236354d67e951340468e020c8afcd2
An An <2236368544@qq.com>
2022-09-09 14:52:37 +0800

    Format code

--- AAChartKitLib/AAOptionsModel/AATitle.h
+++ AAChartKitLib/AAOptionsModel/AATitle.h
@@ -42,7 +42,7 @@ AAPropStatementAndPropSetFuncStatement(copy,   AATitle, NSString *, align)
 AAPropStatementAndPropSetFuncStatement(copy,   AATitle, NSString *, verticalAlign)
 AAPropStatementAndPropSetFuncStatement(strong, AATitle, NSNumber *, x)//标题相对于水平对齐的偏移量，取值范围为：图表左边距到图表右边距，可以是负值，单位px。 默认是：0.
 AAPropStatementAndPropSetFuncStatement(strong, AATitle, NSNumber *, y) //标题相对于垂直对齐的偏移量，取值范围：图表的上边距（chart.spacingTop ）到图表的下边距（chart.spacingBottom），可以是负值，单位是px。默认值和字体大小有关。
-AAPropStatementAndPropSetFuncStatement(assign, AATitle, BOOL          , useHTML) //是否 使用HTML渲染标题。 默认是：false.
+AAPropStatementAndPropSetFuncStatement(assign, AATitle, BOOL      , useHTML) //是否 使用HTML渲染标题。 默认是：false.
 
 @end
 
--- AAChartKitLib/AAOptionsModel/AATitle.m
+++ AAChartKitLib/AAOptionsModel/AATitle.m
@@ -40,7 +40,7 @@ AAPropSetFuncImplementation(AATitle, NSString *, align)
 AAPropSetFuncImplementation(AATitle, NSString *, verticalAlign)
 AAPropSetFuncImplementation(AATitle, NSNumber *, x) //标题相对于水平对齐的偏移量，取值范围为：图表左边距到图表右边距，可以是负值，单位px。 默认是：0.
 AAPropSetFuncImplementation(AATitle, NSNumber *, y) //标题相对于垂直对齐的偏移量，取值范围：图表的上边距（chart.spacingTop ）到图表的下边距（chart.spacingBottom），可以是负值，单位是px。默认值和字体大小有关。
-AAPropSetFuncImplementation(AATitle, BOOL          , useHTML) //是否 使用HTML渲染标题。 默认是：false.
+AAPropSetFuncImplementation(AATitle, BOOL      , useHTML) //是否 使用HTML渲染标题。 默认是：false.
 
 @end
 
