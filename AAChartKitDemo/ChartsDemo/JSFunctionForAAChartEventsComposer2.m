a178f45a08cbcccca5a8bf8b3b570884c79f4535
An An <2236368544@qq.com>
2024-01-29 12:19:05 +0800

    Update JSFunctionForAAChartEventsComposer2.m

--- AAChartKitDemo/ChartsDemo/JSFunctionForAAChartEventsComposer2.m
+++ AAChartKitDemo/ChartsDemo/JSFunctionForAAChartEventsComposer2.m
@@ -12,6 +12,57 @@
 
 @implementation JSFunctionForAAChartEventsComposer2
 
+/*
+ Highcharts.chart('container', {
+     xAxis: {
+         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+     },
+     
+     plotOptions: {
+         series: {
+             events: {
+                 mouseOver: function () {
+                     if(this.hasRun) return;
+                     
+                     var series = this;
+                     var defaultPointIndex = 3;
+                     
+                     if (series.data.length > defaultPointIndex) {
+                         defaultPoint = series.data[defaultPointIndex];
+                         // 模拟点击事件来选中默认点
+                         defaultPoint.select(false);
+                     }
+                     console.log("11111111111");
+                     this.hasRun = true;
+                 }
+             },
+             allowPointSelect: true,
+             // 可以设置选中时的样式，比如边框颜色
+             marker: {
+                 states: {
+                     select: {
+                         fillColor: 'red',
+                         lineWidth: 10,
+                         lineColor: "yellow",
+                         radius:20,
+                     }
+                 }
+             },
+         }
+     },
+     series: [{
+         data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, {
+             y: 29.9,
+             selected: true
+         }]
+     }]
+ });
+ */
+
+//https://api.highcharts.com/highcharts/plotOptions.series.marker.states.hover
+//https://api.highcharts.com/highcharts/plotOptions.series.events.mouseOver
+//https://api.highcharts.com/class-reference/Highcharts.Point#select
+//https://github.com/AAChartModel/AAChartKit/issues/1532
 + (AAOptions *)defaultSelectedAPointForLineChart {
     AADataElement *defaultPointData = AADataElement.new
         .ySet(@29.9)
