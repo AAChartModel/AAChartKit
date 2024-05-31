d8404bfc1a718e76dae4f64698a31a67d408bc62
AAChartModel <2236368544@qq.com>
2024-05-31 16:21:38 +0800

    Fix https://github.com/AAChartModel/AAChartKit-Swift/issues/486  => update property comments

--- AAChartKitLib/AAOptionsModel/AALabels.m
+++ AAChartKitLib/AAOptionsModel/AALabels.m
@@ -46,7 +46,7 @@
 AAPropSetFuncImplementation(AALabels, NSString *, align)//轴标签的对齐方式，可用的值有 "left"、"center" 及 "right"。默认值是根据坐标轴的位置（在图表中的位置）即标签的旋转角度进行智能判断的。 默认是：center.
 AAPropSetFuncImplementation(AALabels, id        , autoRotation)//只针对水平轴有效，允许在防止轴标签重叠时自动旋转轴标签的角度。当空间足够时，轴标签不会被旋转。当图表变小时（主要是宽度变小） ，轴标签开始旋转对应的角度，然后会依次删除间隔的轴标签并尝试旋转数组中的角度。可以通过将此参数设置为 false 来关闭轴标签旋转（这将导致标签自动换行）。 默认是：[-45].
 AAPropSetFuncImplementation(AALabels, NSNumber *, autoRotationLimit)//当每个分类的宽度比该参数的值大很多（像素）时，轴标签将不会被自动旋转，而是以换行的形式展示轴标签。 当轴标签包含多个短词时换行展示轴标签可以使得轴标签有足够的空间，所以设置合理的自动旋转下限是非常有意义的。 默认是：80.
-AAPropSetFuncImplementation(AALabels, NSNumber *, distance)//只针对极地图有效，定义周标签与绘图区边缘的距离。 默认是：15.
+AAPropSetFuncImplementation(AALabels, NSNumber *, distance)//标签距绘图区域周边的像素距离。在笛卡尔图表上，如果设置了 labels.y 设置，则此设置将被覆盖。 默认是：15.
 AAPropSetFuncImplementation(AALabels, BOOL      , enabled)//是否显示坐标轴标签 默认是：true.
 AAPropSetFuncImplementation(AALabels, NSString *, format)//坐标轴格式化字符串。 默认是：{value}.
 //AAPropSetFuncImplementation(AALabels, NSString *, formatter)//坐标轴格式化字符串。 默认是：{value}.
