0ae4d909c2dd1f2700f0be8c7d9b9c30536f4cc3
AAChartModel <2236368544@qq.com>
2024-08-27 15:26:47 +0800

    Fix #1567

---
+++ AAChartKitDemo/Demo/AAOptionsWithJS/JSFunctionForAAPointEventsComposer.h
@@ -0,0 +1,22 @@
+//
+//  JSFunctionForAAPointEventsComposer.h
+//  AAChartKitDemo
+//
+//  Created by AnAn on 2024/8/27.
+//  Copyright © 2024 An An. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AAOptions;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSFunctionForAAPointEventsComposer : NSObject
+
++ (AAOptions *)showTooltipOnlyOnClick;
++ (AAOptions *)customizeCrosshairsStyle;
+
+@end
+
+NS_ASSUME_NONNULL_END
