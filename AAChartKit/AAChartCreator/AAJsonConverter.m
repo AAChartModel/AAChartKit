db8129b91c8b543113c93a8d95d749f411e549b3
An An <2236368544@qq.com>
2025-09-30 18:09:34 +0800

    Update AAJsonConverter.m

--- AAChartKit/AAChartCreator/AAJsonConverter.m
+++ AAChartKit/AAChartCreator/AAJsonConverter.m
@@ -12,28 +12,54 @@
 @implementation AAJsonConverter
 
 + (NSDictionary*)dictionaryWithObjectInstance:(id)objc {
-    unsigned int propsCount;
-    objc_property_t *props = class_copyPropertyList([objc class], &propsCount);
-    NSMutableDictionary *propsDic = [NSMutableDictionary dictionaryWithCapacity:propsCount];
-
-    for (NSUInteger i = 0; i < propsCount; i++) {
-        objc_property_t prop = props[i];
-        NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
-        id value = [objc valueForKey:propName];
-        if (value == nil) {
-            value = [NSNull null];
-            continue;
-        } else {
-            value = [self getObjectInternal:value];
-        }
-        propsDic[propName] = value;
+    if (objc == nil || objc == [NSNull null]) {
+        return @{};
     }
-    free(props);
-    
+
+    NSMutableDictionary *propsDic = [NSMutableDictionary dictionary];
+    Class currentClass = [objc class];
+
+    while (currentClass && currentClass != [NSObject class]) {
+        unsigned int propsCount = 0;
+        objc_property_t *props = class_copyPropertyList(currentClass, &propsCount);
+
+        for (NSUInteger i = 0; i < propsCount; i++) {
+            objc_property_t prop = props[i];
+            NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
+            if (propName.length == 0 || propsDic[propName] != nil) {
+                continue;
+            }
+
+            id value = nil;
+            @try {
+                value = [objc valueForKey:propName];
+            } @catch (NSException *exception) {
+                continue;
+            }
+
+            if (value == nil) {
+                propsDic[propName] = [NSNull null];
+                continue;
+            }
+
+            propsDic[propName] = [self getObjectInternal:value];
+        }
+
+        if (props != NULL) {
+            free(props);
+        }
+
+        currentClass = class_getSuperclass(currentClass);
+    }
+
     return propsDic;
 }
 
 + (id)getObjectInternal:(id)objc {
+    if (objc == nil || objc == [NSNull null]) {
+        return [NSNull null];
+    }
+
     if (   [objc isKindOfClass:[NSString class]]
         || [objc isKindOfClass:[NSNumber class]]
         || [objc isKindOfClass:[NSNull   class]] ) {
@@ -80,11 +106,11 @@
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonObject
                                                        options:kNilOptions
                                                          error:&error];
-    NSString *pureJsonStr =[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    if (error) {
+    if (!jsonData || error) {
         AADetailLog(@"❌❌❌ pure JSONString with JSONObject serialization failed：%@", error);
         return nil;
     }
+    NSString *pureJsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     return pureJsonStr;
 }
 
@@ -94,13 +120,12 @@
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonObject
                                                        options:NSJSONWritingPrettyPrinted
                                                          error:&error];
-    NSString *jsonStr =[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    NSLog(@"----------- console log AAOptions JSON information of AAChartView -----------:\n%@",jsonStr);
-    
-    if (error) {
+    if (!jsonData || error) {
         NSLog(@"❌❌❌ pretty printed JSONString with JSONObject serialization failed：%@", error);
         return nil;
     }
+    NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"----------- console log AAOptions JSON information of AAChartView -----------:\n%@",jsonStr);
     return jsonStr;
 }
 #endif
