bc07572b674b015868e3452d59a9c730e73f15f0
An An <2236368544@qq.com>
2025-10-16 16:52:07 +0800

    Revert the change for the AAChartView 🐛 => Refactor AAJsonConverter property serialization logic
    Simplifies property extraction by removing superclass traversal and improves error handling in JSON serialization methods. Debug logging is now conditional, and nil value handling is streamlined for better performance and readability.

--- AAChartKit/AAChartCreator/AAJsonConverter.m
+++ AAChartKit/AAChartCreator/AAJsonConverter.m
@@ -12,54 +12,28 @@
 @implementation AAJsonConverter
 
 + (NSDictionary*)dictionaryWithObjectInstance:(id)objc {
-    if (objc == nil || objc == [NSNull null]) {
-        return @{};
-    }
+    unsigned int propsCount;
+    objc_property_t *props = class_copyPropertyList([objc class], &propsCount);
+    NSMutableDictionary *propsDic = [NSMutableDictionary dictionaryWithCapacity:propsCount];
 
-    NSMutableDictionary *propsDic = [NSMutableDictionary dictionary];
-    Class currentClass = [objc class];
-
-    while (currentClass && currentClass != [NSObject class]) {
-        unsigned int propsCount = 0;
-        objc_property_t *props = class_copyPropertyList(currentClass, &propsCount);
-
-        for (NSUInteger i = 0; i < propsCount; i++) {
-            objc_property_t prop = props[i];
-            NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
-            if (propName.length == 0 || propsDic[propName] != nil) {
-                continue;
-            }
-
-            id value = nil;
-            @try {
-                value = [objc valueForKey:propName];
-            } @catch (NSException *exception) {
-                continue;
-            }
-
-            if (value == nil) {
-                propsDic[propName] = [NSNull null];
-                continue;
-            }
-
-            propsDic[propName] = [self getObjectInternal:value];
+    for (NSUInteger i = 0; i < propsCount; i++) {
+        objc_property_t prop = props[i];
+        NSString *propName = [NSString stringWithUTF8String:property_getName(prop)];
+        id value = [objc valueForKey:propName];
+        if (value == nil) {
+            value = [NSNull null];
+            continue;
+        } else {
+            value = [self getObjectInternal:value];
         }
-
-        if (props != NULL) {
-            free(props);
-        }
-
-        currentClass = class_getSuperclass(currentClass);
+        propsDic[propName] = value;
     }
-
+    free(props);
+    
     return propsDic;
 }
 
 + (id)getObjectInternal:(id)objc {
-    if (objc == nil || objc == [NSNull null]) {
-        return [NSNull null];
-    }
-
     if (   [objc isKindOfClass:[NSString class]]
         || [objc isKindOfClass:[NSNumber class]]
         || [objc isKindOfClass:[NSNull   class]] ) {
@@ -106,11 +80,13 @@
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonObject
                                                        options:kNilOptions
                                                          error:&error];
-    if (!jsonData || error) {
-        AADetailLog(@"❌❌❌ pure JSONString with JSONObject serialization failed：%@", error);
+    NSString *pureJsonStr =[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    if (error) {
+#if DEBUG
+        NSLog(@"❌❌❌ pure JSONString with JSONObject serialization failed：%@", error);
+#endif
         return nil;
     }
-    NSString *pureJsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
     return pureJsonStr;
 }
 
@@ -120,12 +96,13 @@
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:jsonObject
                                                        options:NSJSONWritingPrettyPrinted
                                                          error:&error];
-    if (!jsonData || error) {
+    NSString *jsonStr =[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"----------- console log AAOptions JSON information of AAChartView -----------:\n%@",jsonStr);
+    
+    if (error) {
         NSLog(@"❌❌❌ pretty printed JSONString with JSONObject serialization failed：%@", error);
         return nil;
     }
-    NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    NSLog(@"----------- console log AAOptions JSON information of AAChartView -----------:\n%@",jsonStr);
     return jsonStr;
 }
 #endif
